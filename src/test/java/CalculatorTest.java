ed4f5131a634fa4a2fc93dd11d2b90e74c88ffef
你的名字 <>
2025-04-16 13:37:37 +0800

    initial commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.add(2, 3));
+    }
+}
